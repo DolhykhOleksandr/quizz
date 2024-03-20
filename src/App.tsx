d2d64715a2e52501769023c83a93694df84e76a8
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2024-03-20 18:28:54 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import MainPage from "./pages/MainPage";
 import Mock from "./components/Mock";
 import About from "./pages/About";
 import UserList from "./pages/UserList";
 import CompanyList from "./pages/CompanyList";
 import UserRegistration from "./pages/UserRegistration";
-import { Route, Routes, Navigate } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom";
 import UserAuthorization from "./pages/UserAuthorization";
 import UserProfile from "./pages/UserProfile";
 import CompanyProfile from "./pages/CompanyProfile";
@@ -14,37 +14,33 @@ import NotFound from "./pages/NotFound";
 import { useSelector } from "react-redux";
 
 export interface RootState {
-    serverStatus: true;
-    testNumber: 0
+  serverStatus: true;
+  testNumber: 0;
 }
 
 function App() {
-    const serverStatus = useSelector((state: RootState) => state.serverStatus);
+  const serverStatus = useSelector((state: RootState) => state.serverStatus);
 
-    const renderContent = () => {
-        return serverStatus ? (
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/userList" element={<UserList />} />
-                <Route path="/userProfile" element={<UserProfile />} />
-                <Route path="/companyList" element={<CompanyList />} />
-                <Route path="/companyProfile" element={<CompanyProfile />} />
-                <Route path="/userRegistration" element={<UserRegistration />} />
-                <Route path="/userAuthorization" element={<UserAuthorization />} />
-                <Route path="/404" element={<NotFound />} />
-                <Route path="/*" element={<Navigate to="/404" />} />
-            </Routes>
-        ) : (
-            <p className="server-error">Server is not available</p>
-        );
-    };
+  if (!serverStatus) {
+    return <p className="server-error">Server is not available</p>;
+  }
 
-    return (
-        <Mock>
-            {renderContent()}
-        </Mock>
-    );
+  return (
+    <Mock>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/userList" element={<UserList />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+        <Route path="/companyList" element={<CompanyList />} />
+        <Route path="/companyProfile" element={<CompanyProfile />} />
+        <Route path="/userRegistration" element={<UserRegistration />} />
+        <Route path="/userAuthorization" element={<UserAuthorization />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to="/404" />} />
+      </Routes>
+    </Mock>
+  );
 }
 
 export default App;
